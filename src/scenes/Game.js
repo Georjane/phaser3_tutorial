3a943311e77c5b775c3e24e614af345b1ca7c87c
Georjane <pzgeorjane@gmail.com>
2021-01-28 14:04:40 +0100

    Create game scene

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,10 +6,10 @@ export default class Game extends Phaser.Scene{
   }
 
   preload(){
-
+    this.load.image('background', 'assets/bg_layer1.png')
   }
 
   create(){
-    
+    this.add.image(240, 320, 'background')
   }
 }
