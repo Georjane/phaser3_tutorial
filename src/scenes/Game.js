7396dd06d14df30a5707f86b2811ab9f893f3c51
Georjane <pzgeorjane@gmail.com>
2021-01-28 14:20:48 +0100

    Created ground platform

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,9 +7,23 @@ export default class Game extends Phaser.Scene{
 
   preload(){
     this.load.image('background', 'assets/bg_layer1.png')
+    this.load.image('platform', 'assets/ground_grass.png')
   }
 
   create(){
-    this.add.image(240, 320, 'background')
+    this.add.image(240, 320, 'background');
+    // this.physics.add.image(240, 320, 'platform').setScale(0.5)
+    const platforms = this.physics.add.staticGroup();
+
+    for (let i = 0; i < 5; ++i) {
+      const x = Phaser.Math.Between(80, 400);
+      const y = 150 * i
+
+      const platform = platforms.create(x, y, 'platform');
+      platform.scale = 0.5;
+
+      const body = platform.body
+      body.updateFromGameObject()
+    }
   }
 }
